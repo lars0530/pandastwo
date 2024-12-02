5bb957ae63eaab246dec0280308a75a31d6f0916
Lars Heimann <lars@larsheimann.de>
2024-12-02 22:16:51 +0100

    add tests for series creation and type checking

--- test/test_series.py
+++ test/test_series.py
@@ -14,22 +14,32 @@ def test_series_creation():
     assert b.data == [True, False, True]
     assert c.data == [1, 2, 3]
     assert d.data == [1.0, 2.0, 3.0]
+    
+def test_series_creation_with_none():
+    a = StringSeries(["a", "b", None])
+    b = BoolSeries([True, False, None])
+    c = IntSeries([1, 2, None])
+    d = FloatSeries([1.0, 2.0, None])
+    assert a.data == ["a", "b", None]
+    assert b.data == [True, False, None]
+    assert c.data == [1, 2, None]
+    assert d.data == [1.0, 2.0, None]
 
-# def test_type_checking_string():
-#     with pytest.raises(Exception):
-#         x = IntSeries(["1", "2", 3])
+def test_type_checking_string():
+    with pytest.raises(Exception):
+        IntSeries(["1", "2", 3])
         
-# def test_type_checking_boolean():
-#     with pytest.raises(Exception):
-#         x = BoolSeries([True, 3, False])
+def test_type_checking_boolean():
+    with pytest.raises(Exception):
+        BoolSeries([True, 3, False])
 
-# def test_type_checking_int():
-#     with pytest.raises(Exception):
-#         x = IntSeries([1, 2, "3"])
+def test_type_checking_int():
+    with pytest.raises(Exception):
+        IntSeries([1, 2, "3"])
 
-# def test_type_checking_float():
-#     with pytest.raises(Exception):
-#         x = IntSeries([1.0, 2.0, "3.0"])
+def test_type_checking_float():
+    with pytest.raises(Exception):
+        IntSeries([1.0, 2.0, "3.0"])
             
     
             
