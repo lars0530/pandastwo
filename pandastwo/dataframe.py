2a9a2201c4519d411c98bcf329f6999f90547120
Lars Heimann <lars@larsheimann.de>
2024-12-08 15:43:21 +0100

    add type check for DataFrame constructor to ensure data is a dictionary

--- pandastwo/dataframe.py
+++ pandastwo/dataframe.py
@@ -8,6 +8,10 @@ from pandastwo.series import Series
 class DataFrame:
     def __init__(self, data: dict[str, Series]) -> None:
         # check data is not empty?
+        # currently, empty data is allowed, should be allowed if "adding" functionality is implemented
+        # --> therefore, no check for empty data
+        if not isinstance(data, dict):
+            raise ValueError("data must be of type dictionary")
         # check all keys are string, all values are Series
         for key, value in data.items():
             if not isinstance(key, str):
